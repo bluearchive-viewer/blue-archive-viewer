9ba6a60d969a16c142ac6a5ded0f8e1f87502caf
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2023-03-16 21:44:37 +0900

    色々追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
-import {config} from 'site.config';
-const inter = Inter({ subsets: ['latin'] })
+import {config} from '@site.config';
 
-export default function Home() {
-return (
-  <AppBar position="static">
-  <Toolbar>
-    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-{config.siteMeta.title}
-    </Typography>
-  </Toolbar>
-</AppBar>
-);
+function Header() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => setOpen(!open)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {config.siteMeta.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        とくぞう様かみ！
+      </Drawer>
+    </>
+  );
 }
+export default Header;
